65e01ba5ed9c83320524284157f12ac648f1116a
ammar <user@Users-MacBook-Pro.local>
2020-05-27 22:02:52 +0100

    implemented event syncing when query service is down

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,19 +5,26 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 //receive event and chain propagate the event to different services
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
   //posts service
   axios.post('http://localhost:4000/events', event);
   //comments service
   axios.post('http://localhost:4001/events', event);
   //query service
   axios.post('http://localhost:4002/events', event);
-  //moderastion service
+  //moderation service
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const axios = require('axios');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-//client will make a request to get all posts
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-//store in a data structure the posts and their corresponding comments
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -39,12 +32,27 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
+//client will make a request to get all posts
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+//store in a data structure the posts and their corresponding comments
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   console.log(posts);
-
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+  const res = await axios.get('http://localhost:4005/events');
+  //on start retrieve all of the events from the even bus
+  for (let event of res.data) {
+    console.log('Processing event: ', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
